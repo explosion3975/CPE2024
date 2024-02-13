14442e94b130fd62689dcb38fdfd309dd33fe1ab
explosion3975 <explosion3975@gmail.com>
2024-02-14 03:56:03 +0800

    2024/02/14

--- UVA10041.cpp
+++ UVA10041.cpp
@@ -4,18 +4,18 @@ int main()
 {
     int total;
     cin >> total;
-    for(int num = 0;num < total;num++)
+    for (int num = 0; num < total; num++)
     {
-        int num1,sum = 0;
+        int num1, sum = 0;
         int array[10000];
         cin >> num1;
-        for(int i = 0;i < num1;i++)
+        for (int i = 0; i < num1; i++)
         {
             cin >> array[i];
         }
-        for(int i = num1 - 1;i > 0;i--)
+        for (int i = num1 - 1; i > 0; i--)
         {
-            for(int ii = 0;ii < i;ii++)
+            for (int ii = 0; ii < i; ii++)
             {
                 if (array[ii] > array[ii + 1])
                 {
@@ -26,7 +26,7 @@ int main()
             }
         }
         int middle = (num1 - 1) / 2;
-        for(int i = 0;i < num1;i++)
+        for (int i = 0; i < num1; i++)
         {
             sum += abs(array[middle] - array[i]);
         }
