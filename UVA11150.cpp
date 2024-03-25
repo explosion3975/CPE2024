aeeddc88b4d2537c51ae103cfabac74931df7150
explosion3975 <explosion3975@gmail.com>
2024-03-26 00:05:21 +0800

    2024/03/25

---
+++ UVA11150.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num;
+    while(cin >> num)
+    {
+        num = num * 3 / 2;
+        cout << num << endl;
+    }
+    return 0;
+}
